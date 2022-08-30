3976ad65c9bf687b6a6e0e877d80bf981d0f3ff4
ChrisIwanowski <bazinga1@gmail.com>
2022-08-30 12:42:05 +0200

    Drugi pkt journey

---
+++ greetings.py
@@ -0,0 +1,18 @@
+
+user_name = input('Please enter your name: ')
+print(f'Hello {user_name}, I am your phone book.')
+
+user_age = (input('Please provide your age: '))
+
+if user_age.isdigit():
+    user_age = int(user_age)
+else:
+   print('That doesn\'t seem to be an integer')
+   exit()
+
+if user_age <= 18:
+    print('You are so young! Life is ahead of you!')
+elif 40 > user_age > 18:
+    print('That\'s a nice age!')
+elif user_age >= 40:
+    print('you must be very wise!')
